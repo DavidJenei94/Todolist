c49d40d1722700b45e06eab69ec076f14812e582
Jenei David <jenei.david1994@gmail.com>
2020-07-02 22:26:00 +0200

    skeleton of todo list

--- app.js
+++ app.js
@@ -4,12 +4,36 @@ const https = require("https");
 
 const app = express();
 
+var items = ["Buy food","Cook food", "Eat food"];
+
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
+
+    var today = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+    
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay: day, newListItems: items});
+
+    console.log(today);
+    console.log(day);
+
+    //res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    items.push(item);
+
+    res.redirect("/");
+});
 
 
 app.listen(3000, function(){
